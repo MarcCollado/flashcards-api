6c20d35606d09cf6595047787bdf1499fe7a8b40
Marc Collado <marc@collado.io>
2018-07-09 18:23:30 +0200

    Add alternative GraphiQL path

--- app.js
+++ app.js
@@ -16,8 +16,15 @@ app.use(
   '/graphql',
   graphqlHTTP((req) => ({
     schema,
+    graphiql: process.env.NODE_ENV === 'development',
+  })),
+);
+
+app.use(
+  '/3p7rp6g1zd3ct2oa7v8h',
+  graphqlHTTP((req) => ({
+    schema,
     graphiql: true,
-    // process.env.NODE_ENV === 'development',
   })),
 );
 
@@ -33,7 +40,6 @@ app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');

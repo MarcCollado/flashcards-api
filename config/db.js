642496da6065dcb56720d162976b6f75c5dde15a
Marc Collado <marc@collado.io>
2018-07-09 17:41:54 +0200

    Add database user and password

--- config/db.js
+++ config/db.js
@@ -5,9 +5,10 @@ const dbParams = {
   dbURL: process.env.DB_URL,
   dbPort: process.env.DB_PORT,
   dbName: process.env.DB_NAME,
-  sessionSecret: process.env.SESSION_KEY,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
 };
-const { dbURL, dbPort, dbName, sessionSecret } = dbParams;
+const { dbURL, dbPort, dbName, dbUser, dbPassword } = dbParams;
 
-mongoose.connect(`mongodb://${dbURL}:${dbPort}/${dbName}`);
+mongoose.connect(`mongodb://${dbUser}:${dbPassword}@${dbURL}:${dbPort}/${dbName}`);
 mongoose.Promise = global.Promise;
